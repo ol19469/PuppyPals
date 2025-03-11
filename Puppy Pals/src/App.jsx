270dfb80ef8688ddc44e79642dfb7261635a119d
ol19469 <ol19469@gmail.com>
2025-03-10 23:48:39 -0500

    finished assigment

--- Puppy Pals/src/App.jsx
+++ Puppy Pals/src/App.jsx
@@ -1,16 +1,38 @@
 import "./App.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
+import "./index.css";
 function App() {
   console.log("puppyList: ", puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
     <div className="App">
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            className="underlined"
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
