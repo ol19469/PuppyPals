91b6f6bb432d8974e230e7b4395db9c505045716
ol19469 <ol19469@gmail.com>
2025-03-10 21:40:17 -0500

    rendered puppylist on screen and logging pupppy list

---
+++ Puppy Pals/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
+function App() {
+  console.log("puppyList: ", puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
+}
+
+export default App;
